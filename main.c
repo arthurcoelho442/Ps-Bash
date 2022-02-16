68da6eb4df6e8713b8047e41ea98890a9920c0a6
Milena Mantovanelli <56406192+Milena0899@users.noreply.github.com>
2022-02-16 00:42:57 -0300

    Add files via upload
    Rodando comandos em background

--- main.c
+++ main.c
@@ -1,7 +1,92 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<signal.h>
+#include<sys/wait.h>
+#include<unistd.h>
+#include<string.h>
+#include <signal.h>
 
-int main(){
-    return 0;
+int read_command(char *cmd[100]){
+    char linha[1024];
+    char *tok;
+    int count =0, i=0;
+
+    //Pega a linha digitada
+    for(;;){
+        int c = fgetc(stdin);
+        linha[count++] = (char) c;
+        if( c == '\n' ) break;
+    }
+    //retorna caso não haja comandos
+    if(count == 1) 
+        return 0;
+    
+    //Divide os comandos caso seja executado mais de um
+    tok = strtok(linha, ";");
+    while(tok){
+        cmd[i++] = strdup(tok);
+        tok = strtok (NULL, ";");
+    }
+    return i;
+}
+
+void treatment_command(char cmd[], char *par[]){
+    int i = 0;
+    char *array[100];
+    char *tok;
+    
+    if(cmd[strlen(cmd)-1] == '\n')
+        cmd[strlen(cmd)-1] = '\0';
+    
+    tok = strtok(cmd, " ");
+    while(tok != NULL){
+        array[i++] = strdup(tok);
+        tok = strtok (NULL, " ");
+    }
+    
+    strcpy(cmd, array[0]);
+    for(int j=0; j < i; j++)
+        par[j] = array[j]; 
+    par[i] = NULL;
+}
+
+void type_prompt(){
+    static int first_time = 1;
+    if(first_time){
+        const char* CLEAR_SCREEN_ANSI =" \e[1;1H\e[2J";
+        write(STDOUT_FILENO,CLEAR_SCREEN_ANSI, 12);
+        first_time=0;
+    }
+    printf("psh>");
+}
+
+int main(int argc, char **argv){
+    char cmd[100], *command[100], *parameters[20];
+    char *envp[]={(char*) "PATH=/bin", 0};
+    int qtdCommand;
+    pid_t pid;
+    
+    while(1){
+        type_prompt();
+        qtdCommand = read_command(command);
+        
+        for(int i=0; i<qtdCommand; i++){
+            treatment_command(command[i], parameters);
+            pid = fork();
+             if(pid!=0){
+                //PROCESSO PAI
+                //printf("\nGPP = %d\n", getpgid(pid));
+                int status;
+                waitpid(-1, &status, WNOHANG);
+            } else {
+                if(qtdCommand == 1)//não vacinado
+                    setpgid(0,0); 
+                //printf("\nGPF = %d\n", getpgid(pid));
+                strcpy(cmd, "/bin/");
+                strcpy(cmd, command[i]);
+                execvp(cmd, parameters);
+            } 
+            if(strcmp(command[i], "term")==0) return 0;
+        }
+    }
+    return EXIT_SUCCESS;
 }
